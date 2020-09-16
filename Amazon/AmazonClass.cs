074ce6bca2e8b9e8a5c75e07e137c28cf456d68a
Maksim <49658752+MaksimLihoi@users.noreply.github.com>
2020-09-16 23:02:35 +0300

    Add files via upload

---
+++ Amazon/AmazonClass.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon
+{
+    class AmazonClass
+    {
+        IWebDriver driver;
+        IWebElement SearchInput, LoginField, PassField, ClickerElement;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:\\discD\\SeleniumChrome");
+            
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://edufpmi.bsu.by/login/index.php";
+        }
+
+        public void StartWork()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                if (System.DateTime.Now.Hour == 8 && System.DateTime.Now.Minute == 17)
+                {
+                    LoginField = driver.FindElement(By.Id("username"));
+                    LoginField.Click();
+                    System.Threading.Thread.Sleep(541);
+                    LoginField.SendKeys("fpm.lihovec");
+                    System.Threading.Thread.Sleep(432);
+                    PassField = driver.FindElement(By.Id("password"));
+                    PassField.Click();
+                    PassField.SendKeys("1823402");
+                    System.Threading.Thread.Sleep(734);
+                    SearchInput = driver.FindElement(By.Id("loginbtn"));
+                    SearchInput.Click();
+                    System.Threading.Thread.Sleep(1231);
+                    flag = false;
+                }
+            }
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            
+            //driver.Close();
+            driver.Quit();
+        }
+
+    }
+}
---
+++ Amazon/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Amazon
+{
+    public partial class Form1 : Form
+    {
+        AmazonClass TestNow = new AmazonClass();
+       
+        // IWebDriver Browser;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            TestNow.startBrowser();
+            TestNow.test();
+         
+            //Browser = new OpenQA.Selenium.Chrome.ChromeDriver("C:\\discD\\SeleniumChrome");
+
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            TestNow.closeBrowser();
+                       
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            TestNow.StartWork();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TestNow.closeBrowser();
+        }
+    }
+}
